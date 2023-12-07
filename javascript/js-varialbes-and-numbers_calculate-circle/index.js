b55bd90c21e8f129495a3312057c24b86f4ce531
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-07 17:56:17 +0100

    Calculate Circle Area and Circumference

---
+++ javascript/js-varialbes-and-numbers_calculate-circle/index.js
@@ -0,0 +1,13 @@
+// Calculate Circle Area and Circumference
+
+let radius = 5;
+let circleArea = Math.PI * Math.pow(radius, 2);
+let circumference = 2 * Math.PI * radius;
+
+console.log(
+  "Area of Circle: ",
+  circleArea,
+  "and",
+  "Circumference of Circle: ",
+  circumference
+);
