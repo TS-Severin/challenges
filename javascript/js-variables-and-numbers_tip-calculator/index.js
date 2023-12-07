9c232379fae1c9594087a8867837ef249f619b2f
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-07 15:42:17 +0100

    add tip calculator

---
+++ javascript/js-variables-and-numbers_tip-calculator/index.js
@@ -0,0 +1,10 @@
+// Tip Calculator
+
+let mealCost = 22.5;
+let tipPercentage = 15;
+
+const tipAmount = mealCost * (tipPercentage / 100);
+
+const totalCost = tipAmount + mealCost;
+
+console.log("The total cost of you bill is: ", totalCost, "euros");
