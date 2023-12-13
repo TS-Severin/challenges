21f46bbe1a02ae99b3b47c8dbf7e113b4219a66a
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-13 19:32:06 +0100

    assign input element to a variable

--- javascript/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -10,3 +10,6 @@ Convert the input value to uppercase when clicking the button
 Hint: Access the value by using `.value` on the input element
 
 */
+
+const input = document.querySelector('[data-js="first-input"]');
+console.log(input.value);
