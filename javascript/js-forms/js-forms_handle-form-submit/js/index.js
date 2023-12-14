4aa4481f194dc502ff608883388bd171496aadd0
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-14 12:41:32 +0100

    solve ageBadnessSum

--- javascript/js-forms/js-forms_handle-form-submit/js/index.js
+++ javascript/js-forms/js-forms_handle-form-submit/js/index.js
@@ -14,4 +14,29 @@ form.addEventListener("submit", (event) => {
   console.log(formElements);
   event.target.reset();
   event.target.elements.firstName.focus();
+
+  // ## Bonus: Calculate and log the age-badness-sum 🤷‍♀️
+
+  // The formula is: `age + badness`
+
+  // Log to the console _(replace the values between the quotes)_:
+
+  // > The age-badness-sum of "firstName" is "age-badness-sum"
+
+  console.log(formElements.firstName.value);
+  console.log(formElements.badness.value);
+  console.log(formElements.age.value);
+
+  function calculateAgeBadnessSum() {
+    const age = formElements.age.value;
+    const badness = formElements.badness.value;
+
+    const ageNumeric = parseFloat(age); // I was stuck here: chat gpt helped me with parseFloat
+    const badnessNumeric = parseFloat(badness); // parseFloat makes the function handle the data as numbers, otherwise it woud be 4 number like: 3556 (age+badness)
+
+    return ageNumeric + badnessNumeric;
+  }
+
+  const result = calculateAgeBadnessSum();
+  console.log(result);
 });
