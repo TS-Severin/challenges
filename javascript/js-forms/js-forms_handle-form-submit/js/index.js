42f0a587b06c2b28371732dd05a02bfcafee7896
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-14 11:28:46 +0100

    log form data, reset and focus on first name

--- javascript/js-forms/js-forms_handle-form-submit/js/index.js
+++ javascript/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+// Log all form data (in object form) into the console in the submit event handler.
+
+// Make sure the form is reset and the First Name input is focused after submission.
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formElements = event.target.elements;
+
+  console.log(formElements);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
