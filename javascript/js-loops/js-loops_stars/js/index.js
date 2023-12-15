a21b8221bd3d04c3f5ba9c866dcc89c2fbaf7462
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-15 21:00:02 +0100

    render stars

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -5,10 +5,15 @@ const starContainer = document.querySelector('[data-js="star-container"]');
 function renderStars() {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
-
   //--v-- your code here --v--
-
-  //--^-- your code here --^--
+  let counter = 1;
+  while (counter <= 5) {
+    var img = document.createElement("img");
+    starContainer.append(img);
+    img.src = "assets/star-empty.svg";
+    counter++;
+    //--^-- your code here --^--
+  }
 }
 
 renderStars();
