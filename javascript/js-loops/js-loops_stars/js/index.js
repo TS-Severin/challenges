28177eec8dbaed86b3292119a2bb6e0091313a02
Tillmann Severin <tillmann.severin@posteo.de>
2023-12-15 21:49:12 +0100

    function for filling stars

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -2,18 +2,21 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
-  // reset the star container before rerendering stars
+function renderStars(filledStars) {
   starContainer.innerHTML = "";
-  //--v-- your code here --v--
+
   let counter = 1;
   while (counter <= 5) {
     var img = document.createElement("img");
     starContainer.append(img);
     img.src = "assets/star-empty.svg";
+
+    if (counter <= filledStars) {
+      img.src = "assets/star-filled.svg";
+    } else {
+      img.src = "assets/star-empty.svg";
+    }
     counter++;
-    //--^-- your code here --^--
   }
 }
-
-renderStars();
+renderStars(3);
