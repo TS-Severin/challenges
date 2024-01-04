fd77fc2b4a9672556f55efc998cdc3a6d3de73a7
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-04 14:34:00 +0100

    save spared files

--- javascript/js-structure_shapes/components/Square/Square.js
+++ javascript/js-structure_shapes/components/Square/Square.js
@@ -1,3 +1,6 @@
+// this is only the definition of a function it isn't called
+// => getRandomColor here is only a local variable
+
 export default function Square(getRandomColor) {
   const square = document.createElement("div");
   square.classList.add("square");
