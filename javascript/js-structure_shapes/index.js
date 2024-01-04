24d60e001616951d17e9a3d42daaf541b5fc4422
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-04 14:35:58 +0100

    update

--- javascript/js-structure_shapes/index.js
+++ javascript/js-structure_shapes/index.js
@@ -1,34 +1,18 @@
 import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
 
 console.clear();
 
-// ### Apply random colors
-
-// Open the [index.js](./index.js). You find 3 elements that are generated: A circle, a square and a pentagon. If one of the elements is clicked the color changes to gray. Let's use the `getRandomColor` function to make the elements more colorful!
-
-// - import the function `getRandomColor` from the [utils/randomColor.js](./utils/randomColor.js) file.
-// - use the function inside all three event listeners and apply the returned color to the backgroundColor.
-
-// > 💡 Hint: if it doesn't work you might need to change something inside the `index.html` regarding the script tag ;)
-
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = getRandomColor();
-});
+// the three function defined in the corresponding folders are called with the actual function getRandomColor as an argument => the function is imported above
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor();
-});
+const circleElement = Circle(getRandomColor);
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
+const squareElement = Square(getRandomColor);
 
-root.append(circle, square, pentagon);
+const pentagonElement = Pentagon(getRandomColor);
+
+root.append(circleElement, squareElement, pentagonElement);
