1d7d71763adf6d6b71d9a8a335d8f11b6258b026
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-09 19:57:10 +0100

    add functions and tests

--- javascript/js-unit-testing/js-unit-testing_people/index.js
+++ javascript/js-unit-testing/js-unit-testing_people/index.js
@@ -2,18 +2,39 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  const peopleWithCertainAge = people.filter((person) => person.age === age);
+  const firstNames = peopleWithCertainAge.map((person) => person.firstName);
+  return firstNames;
+  //return people.filter((person) => person.age === age); => for whole object
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  const oldPeople = people.filter((person) => person.age > age);
+  return getFullNames(oldPeople);
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person) => person.lastName === lastName);
+}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  return people.find((person) => person.id === id);
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > age);
+}
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  const sortedPeople = people.sort((a, b) => b.age - a.age);
+  return getFullNames(sortedPeople);
+}
