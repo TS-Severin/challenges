63d38dff4884e19a69bf720ad1bef5149a5f5ef9
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-15 17:53:26 +0100

    add function

--- react_props/react-props_button/src/App.js
+++ react_props/react-props_button/src/App.js
@@ -2,5 +2,68 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("you clicked me");
+  }
+  return (
+    <Button
+      onClick={handleClick}
+      color={"red"}
+      text={"text"}
+      disabled={false}
+    />
+  );
 }
+
+function Button({ color, text, disabled, onClick }) {
+  return (
+    <button style={{ color: color }} disabled={disabled} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+// old solutions:
+
+// import React from "react";
+// import "./styles.css";
+
+// export default function App({ onClick }) {
+//   function handleClick() {
+//     console.log("you clicked me");
+//   }
+//   return (
+//     <Button
+//       color={"red"}
+//       text={"text"}
+//       disabled={false}
+//       onClick={handleClick}
+//     />
+//   );
+// }
+
+// function Button({ color, text, disabled, onClick }) {
+//   return (
+//     <button style={{ color: color }} disabled={disabled} onClick={onClick}>
+//       {text}
+//     </button>
+//   );
+// }
+
+// import React from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   return <Button color={"red"} text={"text"} disabled={false} />;
+// }
+
+// function Button({ color, text, disabled }) {
+//   function handleClick() {
+//     console.log("you clicked me");
+//   }
+//   return (
+//     <button style={{ color: color }} disabled={disabled} onClick={handleClick}>
+//       {text}
+//     </button>
+//   );
+// }
