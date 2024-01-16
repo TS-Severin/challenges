614a473b7ca5d1c970005df3b3042411b29dc4ad
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-16 15:55:13 +0100

    add avatar and logo

--- react/react_nesting/react-nesting_navigation/src/App.js
+++ react/react_nesting/react-nesting_navigation/src/App.js
@@ -2,31 +2,21 @@ import "./styles.css";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Link from "./components/Link";
-import Image from "./components/Image";
-import avatar from "./img/avatar.jpg";
-import logo from "./img/logo.jpg";
+import Logo from "./components/Logo";
+import Avatar from "./components/Avatar";
 
 export default function App() {
   return (
     <>
       <Header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
+        <Logo />
 
         <Navigation>
           <Link href="#home">Home</Link>
           <Link href="#about">About</Link>
           <Link href="#impressum">Impressum</Link>
         </Navigation>
-
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
+        <Avatar />
       </Header>
       <main>content goes here…</main>
     </>
