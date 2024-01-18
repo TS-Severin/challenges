9d44d21532d8f887822a4cf378341dad712e348e
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-18 12:14:06 +0100

    Add Funcitonality

--- react_states-two/react-state-2_table-reservation/src/App.js
+++ react_states-two/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const addPeople = () => {
+    setPeople(people + 1);
+  };
+
+  function removePeople() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onAddPeople={addPeople} onRemovePeople={removePeople} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
