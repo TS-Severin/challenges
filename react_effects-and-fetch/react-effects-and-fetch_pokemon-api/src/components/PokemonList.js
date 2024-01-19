8d24847aa2de1e92e14909975ddfd99b37d14660
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-19 16:16:20 +0100

    remove button add useEffect function

--- react_effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
+++ react_effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
@@ -1,23 +1,34 @@
 import { useState } from "react";
+import { useEffect } from "react";
+
+// For now the Pokémon are only fetched when we click the button below. Let's change the code so that our app does this automatically when the app renders.
+
+// - Remove the 'Load Pokémon' button.
+// - Change the code below so that the `loadPokemon` function is only executed when the app component is rendered initially`
+
+// _Hint:_ Don't forget the dependency array!
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
 
-  async function loadPokemon() {
-    try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-      const data = await response.json();
-      setPokemon(data.results);
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+        const data = await response.json();
+        setPokemon(data.results);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
+    loadPokemon();
+  }, []);
 
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
+      {/* <button type="button" onClick={loadPokemon}>
         Load Pokémon
-      </button>
+      </button> */}
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
