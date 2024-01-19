966210556f9aaf6ec87711b7e27e44b9c0b42d97
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-19 13:42:23 +0100

    Add functions handleAddTag and handleDeleteTag and according functionality

--- react-state-3/react-state-3_tags/src/App.js
+++ react-state-3/react-state-3_tags/src/App.js
@@ -3,13 +3,26 @@ import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
 
+// - In the `App` component, write a `handleDeleteTag` function which receives one parameter, e.g. `tagToDelete`.
+// - Call the state setter function and remove the tag from the current state.
+// - Pass `handleDeleteTag` to the `List` component using the correct prop name.
+
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    console.log("deleted tag");
+    setTags(tags.filter((tags) => tags !== tagToDelete));
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
