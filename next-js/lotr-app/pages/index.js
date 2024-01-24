c6bbbfaace76fada9e13d3c1736350ca78dc7238
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-24 21:29:27 +0100

    add pages and volume page and content of volumes page

--- next-js/lotr-app/pages/index.js
+++ next-js/lotr-app/pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Link href="/volumes/">
+        <h1>Hello from Next.js</h1>
+      </Link>
     </div>
   );
 }
---
+++ next-js/lotr-app/pages/volumes/index.js
@@ -0,0 +1,11 @@
+import { introduction } from "../../resources/lib/data";
+
+// Overview Page
+
+export default function Volumes() {
+  return (
+    <>
+      <h1>Lord of the Rings</h1>;<p>{introduction}</p>
+    </>
+  );
+}
