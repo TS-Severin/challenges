5869aae71ddc70a985a177a48193836537d37e0a
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-25 13:28:52 +0100

    add if condition for non existing volumes

--- next-js/lotr-app/pages/volumes/[slug].js
+++ next-js/lotr-app/pages/volumes/[slug].js
@@ -13,35 +13,43 @@ export default function VolumeDetail() {
   const index = volumes.findIndex(
     (volume) => volume.slug === router.query.slug
   );
-
-  return (
-    <>
-      <Link href="/volumes">← All Volumes</Link>
-      <h1>{currentVolume.title}</h1>
-      <p>{currentVolume.description}</p>
-      <ul>
-        {currentVolume.books.map((book) => (
-          <li key={book.ordinal}>
-            {book.ordinal}: {book.title}
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={currentVolume.cover}
-        alt="Cover of the volume presented"
-        width={140}
-        height={230}
-      />
-      {index > 0 && (
-        <Link href={`/volumes/${volumes[index - 1].slug}`}>
-          Previous Volume: {volumes[index - 1].title}
-        </Link>
-      )}
-      {index < volumes.length - 1 && (
-        <Link href={`/volumes/${volumes[index + 1].slug}`}>
-          Next Volume: {volumes[index + 1].title}
-        </Link>
-      )}
-    </>
-  );
+  if (!currentVolume) {
+    return (
+      <>
+        <h1>This volume doesn't exist</h1>
+        <Link href="/volumes">← All Volumes</Link>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Link href="/volumes">← All Volumes</Link>
+        <h1>{currentVolume.title}</h1>
+        <p>{currentVolume.description}</p>
+        <ul>
+          {currentVolume.books.map((book) => (
+            <li key={book.ordinal}>
+              {book.ordinal}: {book.title}
+            </li>
+          ))}
+        </ul>
+        <Image
+          src={currentVolume.cover}
+          alt="Cover of the volume presented"
+          width={140}
+          height={230}
+        />
+        {index > 0 && (
+          <Link href={`/volumes/${volumes[index - 1].slug}`}>
+            Previous Volume: {volumes[index - 1].title}
+          </Link>
+        )}
+        {index < volumes.length - 1 && (
+          <Link href={`/volumes/${volumes[index + 1].slug}`}>
+            Next Volume: {volumes[index + 1].title}
+          </Link>
+        )}
+      </>
+    );
+  }
 }
