1b19802edb54627af683ba46cf9699317aa1f162
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-30 19:23:36 +0100

    add interval

--- react/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -3,15 +3,12 @@ import Controls from "../Controls/index";
 import Map from "../Map/index";
 import useSWR from "swr";
 
-// const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function ISSTracker() {
-  const { data, error, isLoading, mutate } = useSWR(
-    "https://api.wheretheiss.at/v1/satellites/25544",
-    fetcher
-  );
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher);
 
   console.log("MY ISS DATA ----->", data);
 
